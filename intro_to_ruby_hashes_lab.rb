db056871dd1d0eaa70e81e03bc260a9521bce05d
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-19 09:44:51 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,13 +34,13 @@ def monopoly_with_third_tier
 			:reading_railroad => {},
 			:pennsylvania_railroad => {},
 			:b_and_o_railroad => {},
-			:shortline => {},
+			:shortline => {}
 		},
 		:rent_in_dollars => {
 			:one_piece_owned => 25,
 			:two_pieces_owned => 50,
 			:three_pieces_owned => 100,
-			:four_pieces_owned => 200,
+			:four_pieces_owned => 200
 		}
 	}
 	}
